23567181ebcdb67c36618102659cd6d818ba7f24
SKarolinav <Skkarolinav@gmail.com>
2019-06-02 21:50:38 +0200

    Updates

---
+++ precache-manifest.34c14b9136a4838d93c29b65cf37284b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0f19c41053bae10766ecea655cfe924",
+    "url": "/exchange-currency-app/index.html"
+  },
+  {
+    "revision": "d7ebc239c82970a90902",
+    "url": "/exchange-currency-app/static/css/main.aaa49ca8.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/exchange-currency-app/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "d7ebc239c82970a90902",
+    "url": "/exchange-currency-app/static/js/main.6cbb53d4.chunk.js"
+  },
+  {
+    "revision": "b66a0e878e561023b289",
+    "url": "/exchange-currency-app/static/js/runtime~main.014f1e16.js"
+  },
+  {
+    "revision": "5cb0aef55a6e2227463dfe2e1ac43534",
+    "url": "/exchange-currency-app/static/media/Wallpaper.5cb0aef5.jpg"
+  }
+]);
